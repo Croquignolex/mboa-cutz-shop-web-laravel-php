be56e58dce0c2ee4a03167bd0f141b66dd16377c
Alex NGOMBOL <gpetitalex10@gmail.com>
2020-09-06 01:20:34 +0100

    Clean public files

--- resources/views/partials/home/menu.blade.php
+++ resources/views/partials/home/menu.blade.php
@@ -1,52 +1,66 @@
-<nav class="navbar navbar-expand-lg fixed-top no-padding-and-margin">
-    <div class="container">
-        {{-- Logo --}}
-        <a class="navbar-brand no-padding-and-margin" href="{{ locale_route('home') }}">
-            <img src="{{ img_asset('logo', 'jpg') }}" alt="..." width="70">
-        </a>
-
-        {{-- Toogle button --}}
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-                aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        {{-- Menu --}}
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-lg-auto">
-                {{-- Menu items --}}
-                <li class="nav-item">
-                    <a href="#home" class="nav-link smoothScroll">{{ trans('page.home') }}</a>
-                </li>
-
-                <li class="nav-item">
-                    <a href="#about" class="nav-link smoothScroll">{{ trans('page.about') }}</a>
-                </li>
-
-                <li class="nav-item">
-                    <a href="#activities" class="nav-link smoothScroll">{{ trans('page.activities') }}</a>
-                </li>
-
-                <li class="nav-item">
-                    <a href="#contact" class="nav-link smoothScroll">{{ trans('page.contact') }}</a>
-                </li>
-            </ul>
-
-            {{-- Menu social media icons --}}
-            <ul class="social-icon ml-lg-3">
-                <li>
-                    <a href="https://www.facebook.com/mboacutz"
-                       class="fa fa-facebook"
-                       target="_blank"
-                    ></a>
-                </li>
-                <li>
-                    <a href="https://www.instagram.com/mboa_cutz"
-                       class="fa fa-instagram"
-                       target="_blank"
-                    ></a>
-                </li>
-            </ul>
+<header class="header_area sticky-header">
+    <div class="main_menu">
+        <nav class="navbar navbar-expand-lg navbar-light main_box">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <a class="navbar-brand logo_h" href="index.html"><img src="img/logo.png" alt=""></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+                    <ul class="nav navbar-nav menu_nav ml-auto">
+                        <li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li>
+                        <li class="nav-item submenu dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                               aria-expanded="false">Shop</a>
+                            <ul class="dropdown-menu">
+                                <li class="nav-item"><a class="nav-link" href="category.html">Shop Category</a></li>
+                                <li class="nav-item"><a class="nav-link" href="single-product.html">Product Details</a></li>
+                                <li class="nav-item"><a class="nav-link" href="checkout.html">Product Checkout</a></li>
+                                <li class="nav-item"><a class="nav-link" href="cart.html">Shopping Cart</a></li>
+                                <li class="nav-item"><a class="nav-link" href="confirmation.html">Confirmation</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item submenu dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                               aria-expanded="false">Blog</a>
+                            <ul class="dropdown-menu">
+                                <li class="nav-item"><a class="nav-link" href="blog.html">Blog</a></li>
+                                <li class="nav-item"><a class="nav-link" href="single-blog.html">Blog Details</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item submenu dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                               aria-expanded="false">Pages</a>
+                            <ul class="dropdown-menu">
+                                <li class="nav-item"><a class="nav-link" href="login.html">Login</a></li>
+                                <li class="nav-item"><a class="nav-link" href="tracking.html">Tracking</a></li>
+                                <li class="nav-item"><a class="nav-link" href="elements.html">Elements</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="nav-item"><a href="#" class="cart"><span class="ti-bag"></span></a></li>
+                        <li class="nav-item">
+                            <button class="search"><span class="lnr lnr-magnifier" id="search"></span></button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <div class="search_input" id="search_input_box">
+        <div class="container">
+            <form class="d-flex justify-content-between">
+                <input type="text" class="form-control" id="search_input" placeholder="Search Here">
+                <button type="submit" class="btn"></button>
+                <span class="lnr lnr-cross" id="close_search" title="Close Search"></span>
+            </form>
         </div>
     </div>
-</nav>
+</header>
