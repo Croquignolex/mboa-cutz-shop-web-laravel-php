3da4e38d2c2048da9b69f4c3497bcbeae8590379
Alex Stéphane NGOMBOL <gpetitalex10@gmail.com>
2020-10-16 14:23:32 +0100

    Improve home banner translation

---
+++ resources/lang/en/home.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Home Language Lines
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+
+    'banner_top_title' => "A unique way to take care of yourself",
+    'banner_title' => "Exclusive offers",
+
+    'booking' => "To make a reservation",
+
+    'calendar_one_title' => "Sunday",
+    'calendar_one_range' => "10:00 AM - 08:00 PM",
+    'calendar_two_title' => "Tuesday - Saturday",
+    'calendar_two_range' => "09:00 AM - 07:30 PM",
+    'calendar_three_title' => "Monday",
+    'calendar_three_range' => "Close",
+
+    'welcome_start' => "Welcome on",
+    'welcome_end' => "your ultra modern men aesthetic babershop",
+    'welcome_description' => "We are :name, an ultra modern CAMEROONIAN babershop specialized for Afro, Asian and European men. We are a concept, a philosophy, another way of seeing the haircut",
+
+];
---
+++ resources/lang/fr/home.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Home Language Lines
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+
+    'banner_top_title' => "Une façon unique de prendre soin de vous",
+    'banner_title' => "Des offres exclusives",
+    'booking' => "Faire une reservation",
+
+    'calendar_one_title' => "Dimanche",
+    'calendar_one_range' => "10:00 - 20:00",
+    'calendar_two_title' => "Mardi - Samedi",
+    'calendar_two_range' => "09:00 - 19:30",
+    'calendar_three_title' => "Lundi",
+    'calendar_three_range' => "Fermé",
+
+    'welcome_start' => "Bienvenue sur",
+    'welcome_end' => "votre salon esthétique pour homme ultra moderne",
+    'welcome_description' => "Nous somme :name, un babershop CAMEROUNAIS ultra moderne spécialisé pour hommes Afro, Asiatique et Européen. Nous sommes un concept, une philosphie, une autre façon de voire la coupe de cheveux",
+
+];
