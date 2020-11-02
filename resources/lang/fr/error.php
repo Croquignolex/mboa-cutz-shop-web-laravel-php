94b8211eefa5365245a315e32965cd7c9bfec497
Alex Stéphane NGOMBOL <gpetitalex10@gmail.com>
2020-11-02 14:38:17 +0100

    Improve error messages

--- resources/lang/fr/error.php
+++ resources/lang/fr/error.php
@@ -10,20 +10,20 @@ return [
     |
     */
 
-    '404_title' => 'Page introuvable',
-    '404_message' => 'Désolé, la page demandé est inaccéssible',
+    '404_title' => "Page introuvable",
+    '404_message' => "Désolé, la page demandé est inaccéssible",
 
-    '419_title' => 'Page expirée',
-    '419_message' => 'La page a expirée du faite de l\'inactivité',
+    '419_title' => "Page expirée",
+    '419_message' => "La page a expirée du faite de l'inactivité",
 
-    '429_title' => 'Error',
-    '429_message' => 'Too many requests',
+    '429_title' => "Erreur",
+    '429_message' => 'Trop de requêtes',
 
-    '500_title' => 'Error',
-    '500_message' => 'Whoops, looks like something went wrong',
+    '500_title' => "Erreur",
+    '500_message' => "Whoops, on dirait que quelque chose s'est mal passé",
 
-    '503_title' => 'Service Unavailable',
-    '503_message' => 'Be right back',
+    '503_title' => "Service indisponible",
+    '503_message' => "Essayez plus tard",
 
-    'return' => 'Retour à la page d\'accueil',
+    'return' => "Retour à la page d'accueil",
 ];
