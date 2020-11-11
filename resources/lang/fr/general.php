976c06578241b1bbf20ab40b316b03beda986c24
Alex Stéphane NGOMBOL <gpetitalex10@gmail.com>
2020-11-11 12:44:45 +0100

    Privacy policy page

--- resources/lang/fr/general.php
+++ resources/lang/fr/general.php
@@ -46,4 +46,17 @@ return [
     'generic_error' => "Certaines valeurs sont incorectes",
     'connect_before_comment' => "Vous devez vous connecter afin de pouvoir laisser un commentaire",
 
+    // ******************************* PRIVACY POLICY ******************************* //
+    'terms' => "Termes",
+    'terms_description' => "En accédant à ce site Web, vous acceptez d'être lié par les présentes Conditions générales d'utilisation du site Web, toutes les lois et réglementations applicables, et acceptez que vous êtes responsable du respect des lois locales applicables. Si vous n'êtes pas d'accord avec l'une de ces conditions, il vous est interdit d'utiliser ou d'accéder à ce site. Le matériel contenu dans ce site Web est protégé par le droit d'auteur et le droit des marques applicables",
+    'collected_info' => "Informations collectées",
+    'collected_info_description' => "Lorsque vous interagissez avec nous à travers le site, nous pouvons recueillir des données personnelles et d'autres informations auprès de vous, comme décrit ci-dessous : Les données personnelles que vous fournissez sur le site : Nous recueillons des données personnelles auprès de vous lorsque vous fournissez volontairement ces informations, par exemple lorsque vous nous contacter, pour l'accès aux services de :company ou d'utiliser certains services :company. Où :company recueille des données personnelles, nous faisons un effort pour fournir un lien vers cette politique de confidentialité. En nous fournissant volontairement des données personnelles, vous consentez à notre utilisation de celles-ci conformément à la présente Politique de confidentialité",
+    'security' => "Sécurité",
+    'security_description' => ":company prend des mesures raisonnables pour protéger les données personnelles fournies par le biais du site et de la perte, l'abus et l'accès non autorisé, la divulgation, l'altération ou la destruction. Cependant, par Internet ou par e-mail, la transmission n'est jamais complètement sûre ou sans erreur. En particulier, le e-mail envoyé à partir de ce site ne peut pas être sécurisé. Par conséquent, vous devez prendre un soin particulier dans le choix des informations que vous nous envoyez par e-mail. S'il vous plaît garder cela à l'esprit lors de la divulgation de données personnelles à :company via Internet",
+    'access_info' => "Accès à l'information",
+    'access_info_description' => "Pour conserver vos données personnelles exactes, à jour et complet, s'il vous plaît nous contacter comme indiqué ci-dessous. Nous prendrons des mesures raisonnables pour mettre à jour ou de corriger les données personnelles en notre possession que vous avez déjà soumis via le Site. S'il vous plaît aussi se sentir libre de nous contacter si vous avez des questions concernant cette politique de confidentialité de :company ou les pratiques d'information de ce site. Vous pouvez nous contacter via l’adresse :mail",
+
+    // ******************************* TERMS AND CONDITIONS ******************************* //
+
+
 ];
