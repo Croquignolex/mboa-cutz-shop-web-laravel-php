e5d3dc9bd32c24faa88e315a8cb47a87c21c16f2
Alex Stéphane NGOMBOL <gpetitalex10@gmail.com>
2020-11-15 11:10:15 +0100

    Improve picture model

--- app/Enums/ImagePath.php
+++ app/Enums/ImagePath.php
@@ -7,5 +7,6 @@ class ImagePath
     const USER_DEFAULT_IMAGE_PATH = 'users';
     const PRODUCT_DEFAULT_IMAGE_PATH = 'products';
     const ARTICLE_DEFAULT_IMAGE_PATH = 'articles';
+    const PICTURE_DEFAULT_IMAGE_PATH = 'pictures';
     const TESTIMONIAL_DEFAULT_IMAGE_PATH = 'testimonials';
 }
